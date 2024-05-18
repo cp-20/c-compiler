0216e96734b906a0b871574bb314e55bab66da06
cp-20 <n.naoki.1587@gmail.com>
2024-05-19 00:37:19 +0900

    update target

--- generator.c
+++ generator.c
@@ -309,7 +309,7 @@ void generate_header() {
       "target datalayout = "
       "\"e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-"
       "S128\"\n");
-  printf("target triple = \"x86_64-unknown-linux-gnu\"\n");
+  printf("target triple = \"x86_64-pc-linux-gnu\"\n");
   printf("\n");
 
   // 最後の出力用の文字列
